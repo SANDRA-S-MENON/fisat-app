e7e629c4a0170f9a23f14346d9d519a8b86cdb6f
SANDRA-S-MENON <sandramenon2002@gmail.com>
2024-06-09 23:04:23 +0530

    add jsx created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import AddStudent from './components/AddStudent';
+import SearchStudent from './components/SearchStudent';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
     <div>
       <AddStudent/>
+      <SearchStudent/>
+      <ViewAll/>
       
     </div>
   );
